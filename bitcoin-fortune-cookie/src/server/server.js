831099048751a78024c44969c40bcc146e2c8c34
BitcoinAcolyte <fb.rider@hotmail.com>
2020-12-12 13:05:30 +0900

    removing twitter post's for testing

--- bitcoin-fortune-cookie/src/server/server.js
+++ bitcoin-fortune-cookie/src/server/server.js
@@ -12,15 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // redirecting requests that come in on http
-app.use(function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect("https://" + req.headers.host + req.url);
-  }
-});
+// app.use(function (req, res, next) {
+//   if (req.secure) {
+//     // request was via https, so do no special handling
+//     next();
+//   } else {
+//     // request was via http, so redirect to https
+//     res.redirect("https://" + req.headers.host + req.url);
+//   }
+// });
 
 require("./payment-routes")(app);
 
@@ -41,17 +41,17 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../", "../", "build", "index.html"));
 });
 
-const options = {
-  key: fs.readFileSync(
-    "/etc/letsencrypt/live/bitcoinfortunecookie.com/privkey.pem",
-    "utf8"
-  ),
-  cert: fs.readFileSync(
-    "/etc/letsencrypt/live/bitcoinfortunecookie.com/fullchain.pem",
-    "utf8"
-  ),
-};
-https.createServer(options, app).listen(443);
+// const options = {
+//   key: fs.readFileSync(
+//     "/etc/letsencrypt/live/bitcoinfortunecookie.com/privkey.pem",
+//     "utf8"
+//   ),
+//   cert: fs.readFileSync(
+//     "/etc/letsencrypt/live/bitcoinfortunecookie.com/fullchain.pem",
+//     "utf8"
+//   ),
+// };
+// https.createServer(options, app).listen(443);
 
 app.listen(80);
 console.log("Yip Yip! Listening on port 80");
